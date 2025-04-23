25e845aa5f5a604dd99f8f8aed40e8cab7d6c729
Nikola Najic <nnajic1@gmail.com>
2025-04-23 13:14:19 +0200

    Верзија 1. Искуцан је код али треба да се провери на моторима.

--- main/main.ino
+++ main/main.ino
@@ -1,31 +1,113 @@
-#include "Keypad.h"
+#define step1 22
+#define dir1 23
+#define step2 24
+#define dir2 25
+#define step3 26
+#define dir3 27
+#define step4 28
+#define dir4 29
 
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
-char keys[ROWS][COLS] =
- {{'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}};
-byte rowPins[ROWS] = {
-  5, 6, 7, 8}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {
-  9, 10, 11, 12}; //connect to the column pinouts of the keypad
+#define step5 30
+#define dir5 31
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+#define RX 0
+#define TX 1
 
-void setup()
-{
-  Serial.begin(9600);
-  delay(1000);
+#define servo_levo 32
+#define servo_desno 33
+#define servo_grab_grab 34
+#define servo_vakuum 35
+
+const int t = 500;
+
+void setup() {
+  pinMode(step1, OUTPUT);
+  pinMode(dir1, OUTPUT);
+  pinMode(step2, OUTPUT);
+  pinMode(dir2, OUTPUT);
+  pinMode(step3, OUTPUT);
+  pinMode(dir3, OUTPUT);
+  pinMode(step4, OUTPUT);
+  pinMode(dir4, OUTPUT);
+  pinMode(step5, OUTPUT);
+  pinMode(dir5, OUTPUT);
+
+  digitalWrite(step1, LOW);
+  digitalWrite(dir1, LOW);
+  digitalWrite(step2, LOW);
+  digitalWrite(dir2, LOW);
+  digitalWrite(step3, LOW);
+  digitalWrite(dir3, LOW);
+  digitalWrite(step4, LOW);
+  digitalWrite(dir4, LOW);
+  digitalWrite(step5, LOW);
+  digitalWrite(dir5, LOW);
 }
 
-void loop()
+void loop() {
+  OneStep(1);
+  Dir0(1);
+  OneStep(1);
+  Dir1(1);
+}
+
+void Dir0(int i)
 {
-  char key = keypad.getKey();
-  if (key != NO_KEY)
+  if (i == 1) digitalWrite(dir1, LOW);
+  if (i == 2) digitalWrite(dir2, LOW);
+  if (i == 3) digitalWrite(dir3, LOW);
+  if (i == 4) digitalWrite(dir4, LOW);
+  if (i == 5) digitalWrite(dir5, LOW);
+}
+
+void Dir1(int i)
+{
+  if (i == 1) digitalWrite(dir1, HIGH);
+  if (i == 2) digitalWrite(dir2, HIGH);
+  if (i == 3) digitalWrite(dir3, HIGH);
+  if (i == 4) digitalWrite(dir4, HIGH);
+  if (i == 5) digitalWrite(dir5, HIGH);
+}
+
+void OneStep(int i)
+{
+  if (i == 1)
   {
-    Serial.print(key);
-    delay(100);
+    digitalWrite(step1, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step1, LOW);
+    delayMicroseconds(t);
+  }
+
+  else if (i == 2)
+  {
+    digitalWrite(step2, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step2, LOW);
+    delayMicroseconds(t);
+  }
+
+  else if (i == 3)
+  {
+    digitalWrite(step3, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step3, LOW);
+    delayMicroseconds(t);
+  }
+
+  else if (i == 4)
+  {
+    digitalWrite(step4, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step4, LOW);
+    delayMicroseconds(t);
+  }
+
+  else if (i == 5)
+  {
+    digitalWrite(step5, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step5, LOW);
+    delayMicroseconds(t);
   }
 }
