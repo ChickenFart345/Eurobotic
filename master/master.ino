fb0f789355fb7107dc7797e845144c8c8705cef0
Uroš Branko Aleksić <urosaleksic345@gmail.com>
2025-04-24 16:07:49 +0200

    Код са контролом мотора за кретање. Овај код није тестиран. Комит пре него што је Јаје отишао кући...

--- master/master.ino
+++ master/master.ino
@@ -1,13 +1,37 @@
-int x = 0;
+#define X1 A0
+#define Y1 A1
+#define X2 A2
+#define Y2 A3
+#define B1 6
+#define B2 7
 
 void setup() {
   Serial.begin(38400);
 
-  pinMode(A0, INPUT);
+  pinMode(X1, INPUT);
+  pinMode(Y1, INPUT);
+  pinMode(X2, INPUT);
+  pinMode(Y2, INPUT);
+  pinMode(B1, INPUT_PULLUP);
+  pinMode(B2, INPUT_PULLUP);
 }
 
+bool ready = false;
 void loop() {
-  x = analogRead(A0);
-  Serial.println(x);
-  delay(10);
+  if (Serial.available() > 0){
+    char r = Serial.read();
+    if (r == 'r') ready = true;
+  }
+
+  if (ready) {
+    int x1 = analogRead(X1);
+    int y1 = analogRead(Y1);
+    int x2 = analogRead(X2);
+    int y2 = analogRead(Y2);
+    int b1 = digitalRead(B1);
+    int b2 = digitalRead(B2);
+
+    Serial.println(x1 + ',' + y1 + ',' + x2 + ',' + y2 + ',' + b1 + ',' + b2);
+    ready = false;
+  }
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -1,16 +1,56 @@
 #include <String.h>
 
-int x;
+#define step1 22
+#define dir1 23
+#define step2 24
+#define dir2 25
+#define step3 26
+#define dir3 27
+#define step4 28
+#define dir4 29
+
+#define step5 30
+#define dir5 31
+
+#define RX 0
+#define TX 1
+
+#define servo_levo 32
+#define servo_desno 33
+#define servo_grab_grab 34
+#define servo_vakuum 35
+
+int x1, y1, x2, y2, b1, b2;
 String receivedData = "";
+const int t = 500;
+const int speed = 10;
 
 void setup() {
   Serial.begin(38400);
 
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-}
-void parseData(String data) {
-  x = data.toInt();
+  pinMode(step1, OUTPUT);
+  pinMode(dir1, OUTPUT);
+  pinMode(step2, OUTPUT);
+  pinMode(dir2, OUTPUT);
+  pinMode(step3, OUTPUT);
+  pinMode(dir3, OUTPUT);
+  pinMode(step4, OUTPUT);
+  pinMode(dir4, OUTPUT);
+  pinMode(step5, OUTPUT);
+  pinMode(dir5, OUTPUT);
+
+  digitalWrite(step1, LOW);
+  digitalWrite(dir1, LOW);
+  digitalWrite(step2, LOW);
+  digitalWrite(dir2, LOW);
+  digitalWrite(step3, LOW);
+  digitalWrite(dir3, LOW);
+  digitalWrite(step4, LOW);
+  digitalWrite(dir4, LOW);
+  digitalWrite(step5, LOW);
+  digitalWrite(dir5, LOW);
+
+  Serial.print('r');
 }
 
 void loop() {
@@ -19,26 +59,147 @@ void loop() {
     if (c == '\n') {
       parseData(receivedData);
       receivedData = "";
+      Serial.print('r');
     } 
     else receivedData += c;
   }
 
-  if(x>812)
-  {
-    digitalWrite(7, HIGH);
+  int stanje = CalculateState();
 
-    digitalWrite(6, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(6, LOW);
-    delayMicroseconds(500);
+  if (stanje == 1) {
+    Dir0(2);
+    Dir1(3);
+    Step(2);
+    Step(3);
   }
-  if(x<212)
-  {
-    digitalWrite(7, LOW);
+  else if (stanje == 2) {
+    Dir1(2);
+    Dir0(3);
+    Step(2);
+    Step(3);
+  }
+  else if (stanje == 3) {
+    Dir0(1);
+    Dir1(4);
+    Step(1);
+    Step(4);
+  }
+  else if (stanje == 4) {
+    Dir1(1);
+    Dir0(4);
+    Step(1);
+    Step(4);
+  }
+  else if (stanje == 5) {
+    Dir0(1);
+    Dir0(2);
+    Dir0(3);
+    Dir0(4);
+    Step(1);
+    Step(2);
+    Step(3);
+    Step(4);
+  }
+  else if (stanje == 6) {
+    Dir1(1);
+    Dir1(2);
+    Dir1(3);
+    Dir1(4);
+    Step(1);
+    Step(2);
+    Step(3);
+    Step(4);
+  }
+}
 
-    digitalWrite(6, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(6, LOW);
-    delayMicroseconds(500);
+int CalculateState(){
+  // u slucaju da su pinovi od joystick-a okrenuti na gore
+  if (x1 < 150) return 1; // napred
+  if (x1 > 873) return 2; // nazad
+  if (y1 < 150) return 3; // levo
+  if (y1 > 873) return 4; // desno
+  if (y2 < 150) return 5; // pivot levo
+  if (y2 > 873) return 6; // pivot desno
+  else return 0;
+}
+
+void parseData(String data) {
+  int z1 = data.indexOf(',');
+  int z2 = data.indexOf(',', z1 + 1);
+  int z3 = data.indexOf(',', z2 + 1);
+  int z4 = data.indexOf(',', z3 + 1);
+  int z5 = data.indexOf(',', z4 + 1);
+
+  x1 = data.substring(0, z1).toInt();
+  y1 = data.substring(z1 + 1, z2).toInt();
+  x2 = data.substring(z2 + 1, z3).toInt();
+  y2 = data.substring(z3 + 1, z4).toInt();
+  b1 = data.substring(z4 + 1, z5).toInt();
+  b2 = data.substring(z5 + 1).toInt();
+
+}
+
+void Dir0(int i)
+{
+  if (i == 1) digitalWrite(dir1, LOW);
+  if (i == 2) digitalWrite(dir2, LOW);
+  if (i == 3) digitalWrite(dir3, LOW);
+  if (i == 4) digitalWrite(dir4, LOW);
+  if (i == 5) digitalWrite(dir5, LOW);
+}
+
+void Dir1(int i)
+{
+  if (i == 1) digitalWrite(dir1, HIGH);
+  if (i == 2) digitalWrite(dir2, HIGH);
+  if (i == 3) digitalWrite(dir3, HIGH);
+  if (i == 4) digitalWrite(dir4, HIGH);
+  if (i == 5) digitalWrite(dir5, HIGH);
+}
+
+void OneStep(int i)
+{
+  if (i == 1)
+  {
+    digitalWrite(step1, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step1, LOW);
+    delayMicroseconds(t);
   }
+
+  else if (i == 2)
+  {
+    digitalWrite(step2, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step2, LOW);
+    delayMicroseconds(t);
+  }
+
+  else if (i == 3)
+  {
+    digitalWrite(step3, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step3, LOW);
+    delayMicroseconds(t);
+  }
+
+  else if (i == 4)
+  {
+    digitalWrite(step4, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step4, LOW);
+    delayMicroseconds(t);
+  }
+
+  else if (i == 5)
+  {
+    digitalWrite(step5, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(step5, LOW);
+    delayMicroseconds(t);
+  }
+}
+
+void Step(int i){
+  for (int j; j < speed; j++) OneStep(i);
 }
